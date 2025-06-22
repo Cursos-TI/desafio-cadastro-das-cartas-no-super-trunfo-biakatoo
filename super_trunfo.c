da08fb9a6333d1124e72caed5d3ad51eb736aa40
Núbia Katoo <n.katoo@hotmail.com>
2025-06-22 19:38:44 +0000

    commit atualizado

--- super_trunfo.c
+++ super_trunfo.c
@@ -68,6 +68,16 @@ int main () {
     printf("Informe o número de pontos turísticos: \n");
     scanf("%d", &pontosTuristicos2);
 
+    //Cáculos Carta 1
+    float densidade1, pibPerCapita1;
+    densidade1 = populacao1 / area1;
+    pibPerCapita1 = (pib1 * 1000000000) / populacao1;
+
+    //Cáculos Carta 2
+    float densidade2, pibPerCapita2;
+    densidade2 = populacao2 / area2;
+    pibPerCapita2 = (pib2 * 1000000000) / populacao2;
+    
     // Dados Carta 1
     printf("\nCarta 1:\n");
 
@@ -75,9 +85,11 @@ int main () {
     printf("Código: %s\n", codigo1);
     printf("Cidade: %s\n", nomeCidade1);
     printf("População %d\n", populacao1);
-    printf("Aréa: %.2f\n", area1);
-    printf("PIB: %.2f\n", pib1);
+    printf("Aréa: %.2f Km²\n", area1);
+    printf("PIB: %.2f bilhões de reais\n", pib1);
     printf("Pontos Turísticos: %d\n", pontosTuristicos1);
+    printf("Densidade populacional: %.2f hab/Km²\n", densidade1);
+    printf("PIB per Capita: %.2f reais\n", pibPerCapita1);
 
     // Dados Carta 2
     printf("\nCarta 2:\n");
@@ -86,9 +98,11 @@ int main () {
     printf("Código: %s\n", codigo2);
     printf("Cidade: %s\n", nomeCidade2);
     printf("População %d\n", populacao2);
-    printf("Aréa: %.2f\n", area2);
-    printf("PIB: %.2f\n", pib2);
+    printf("Aréa: %.2f Km²\n", area2);
+    printf("PIB: %.2f bilhões de reais\n", pib2);
     printf("Pontos Turísticos: %d\n", pontosTuristicos2);
+    printf("Densidade populacional: %.2f hab/Km²\n", densidade2);
+    printf("PIB per Capita: %.2f reais\n", pibPerCapita2);
 
     return 0;
 
